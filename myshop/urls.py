fb9c560d6921b7574867f5d196b8a6ab72ec8e62
Vik Sir <vsirotkin15@gmail.com>
2023-12-01 12:01:45 +0300

    setup settings in myshop

--- myshop/urls.py
+++ myshop/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for shop project.
+URL configuration for myshop project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,9 +14,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls import static
+
 from django.contrib import admin
-from django.urls import path
+
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# apps-urls
+urlpatterns += [
+    path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
+    path('payment/', include('payment.urls', namespace='payment')),
+    path('', include('shop.urls', namespace='shop')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
